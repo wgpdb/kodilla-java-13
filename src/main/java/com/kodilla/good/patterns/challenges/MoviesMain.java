be22a3b49842e6c28f704f040aaa693302057e29
Dariusz <dariusz.borek@gmail.com>
2022-04-04 14:06:01 +0200

    13.1 First commit

---
+++ src/main/java/com/kodilla/good/patterns/challenges/MoviesMain.java
@@ -0,0 +1,19 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MoviesMain {
+
+    public static void main(String[] args) {
+
+        String movieTitlesStream;
+
+        MovieStore movieStore = new MovieStore();
+
+        movieTitlesStream = movieStore.getMovies().entrySet().stream()
+                .flatMap(s -> s.getValue().stream())
+                .collect(Collectors.joining("!"));
+
+        System.out.println(movieTitlesStream);
+    }
+}
