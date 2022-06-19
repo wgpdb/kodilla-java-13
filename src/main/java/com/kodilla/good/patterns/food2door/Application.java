17686d3f78f100026948b5a6406d140861068f83
wgpdb <dariusz.borek@gmail.com>
2022-06-19 20:54:35 +0200

    13.4 Updated OrderProcessor and Application

--- src/main/java/com/kodilla/good/patterns/food2door/Application.java
+++ src/main/java/com/kodilla/good/patterns/food2door/Application.java
@@ -6,9 +6,16 @@ public class Application {
 
         Product organicApple = new Product("Organic Apple", 5.99);
         Product organicPear = new Product("Organic Pear", 8.59);
+        Product glutenFreeBread = new Product("Gluten Free Bread", 10.50);
 
         Supplier extraFoodShop = new Supplier("Extra Food Shop",
                 new SupplierStockRepository(organicApple, 200));
+
+        Supplier healthyShop = new Supplier("Healthy Shop",
+                new SupplierStockRepository(organicApple, 250));
+
+        Supplier glutenFreeShop = new Supplier("Gluten Free Shop",
+                new SupplierStockRepository(glutenFreeBread, 50));
 
         Supplier newSupplier = new Supplier();
         newSupplier.onboardNewSupplier("Organic Farmers Market",
